e0e0e1483ca20118fb4cf5afea16b1ed71b03991
João Mago <joao.pedro@natahouse.com>
2022-03-26 19:21:26 -0300

    feat: add mock server for bank settlement

---
+++ mockserver/server.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('mockserver/db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  console.log('POST request listener');
+  if (req.method === 'POST' && req.url === '/paymentOrders') {
+    const randomResponse = Math.floor(Math.random() * 4);
+
+    const response = {
+      internalId: 'mocked123',
+    };
+
+    switch (randomResponse) {
+      case 0:
+        response.status = 'CREATED';
+        break;
+      case 1:
+        response.status = 'APPROVED';
+        break;
+      case 2:
+        response.status = 'SCHEDULED';
+        break;
+      case 3:
+        response.status = 'REJECTED';
+        break;
+    }
+
+    res.json(response);
+  } else {
+    next();
+  }
+});
+
+server.use(router);
+
+server.listen(5000, () => {
+  console.log('mockserver is running');
+});
